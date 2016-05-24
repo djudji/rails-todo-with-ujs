b7da72cdffcb22e85f08b879ba50b1a5566f03dd
Zlatko Alomerovic <zlatko@miavision.net>
2016-05-24 10:47:25 +0200

    Added correct relatinship for models

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,3 @@
 class Tag < ApplicationRecord
-	belongs_to :user
 	has_and_belongs_to_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
-	has_many :task_tags
-	has_many :tags, :through => :task_tags
+	#has_many :task_tags
+	#has_many :tags, :through => :task_tags
+		belongs_to :user
+		has_and_belongs_to_many :tags
 end

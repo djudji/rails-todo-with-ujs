91fb079883fa8d1b73ce4ff5c2d73dd93cea7e1a
Zlatko Alomerovic <zlatko@miavision.net>
2016-05-24 13:55:00 +0200

    Completed Tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,49 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: [:show, :edit, :update, :destroy]
 	def index
 		@tasks = current_user.tasks
 	end
-	def new
+
+	def show
 	end
+
+	def new
+		@task = Task.new
+	end
+
 	def edit
 	end
-	def create
 
+	def create
+		@task = Task.new(task_params)
+		if task.save
+			redirect_to @task, notice: "Task successfully created!"
+		else
+			render :new
+		end
 	end
-	def delete
+
+	def update
+		if task.update(task_params)
+			redirect_to @task, notice: "Task successfully updated!"
+		else
+			render :edit
+		end
 	end
+
+	def destroy
+		if @task.destroy
+			redirect_to tasks_url, notice: "Task successfully deleted!"
+		end
+	end
+
+	private
+
+		def set_task
+			@task = Task.find(params[:id])
+		end
+
+		def task_params
+			params.require(:task).permit(:title, :description, :finished)
+		end
 end

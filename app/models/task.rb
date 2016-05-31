323b60d578cd58bf7485ea15885c3e02d217af0f
Zlatko Alomerovic <zlatko@miavision.net>
2016-05-31 16:22:27 +0200

    Small model improvements

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,25 +1,24 @@
 class Task < ApplicationRecord
-	#has_many :task_tags
-	#has_many :tags, :through => :task_tags
-		belongs_to :user
-		has_and_belongs_to_many :tags
+	
+	belongs_to :user
+	has_and_belongs_to_many :tags
 
-		scope :important, -> { where(important: true) }
-		scope :starred, -> { where(starred: true) }
-		scope :finished, -> { where(finished: true) }
+	scope :important, -> { where(important: true) }
+	scope :starred, -> { where(starred: true) }
+	scope :finished, -> { where(finished: true) }
 
-		def all_tags=(names)
-			self.tags = names.split(",").map do |name|
-				Tag.where(name: name.strip).first_or_create!
-			end
+	def all_tags=(names)
+		self.tags = names.split(",").map do |name|
+			Tag.where(name: name.strip).first_or_create!
 		end
+	end
 
-		def all_tags
-			self.tags.map(&:name).join(", ")
-		end
+	def all_tags
+		self.tags.map(&:name).join(", ")
+	end
 
-		def self.tagged_with(name)
-			Tag.find_by_name!(name).tasks
-		end
+	def self.tagged_with(name)
+		Tag.find_by_name!(name).tasks
+	end
 		
 end
